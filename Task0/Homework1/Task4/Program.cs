b39a11f143b670779cc86baaf8c98b6fdd3aefd7
Ksenia <knopik-ksu@mail.ru>
2022-12-19 21:28:25 +0500

    Task4 is done

--- Task0/Homework1/Task4/Program.cs
+++ Task0/Homework1/Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+WriteLine("Введите третье число: ");
+int c = int.Parse(ReadLine()!);
+int max = a;
+if(b > max) max = b;
+if(c > max) max = c;
+WriteLine($"Максимальное из этих чисел: {max}");
