c00616d1572f1ce8bf8ebd41f1bcadcf68273fe3
Ksenia <knopik-ksu@mail.ru>
2022-12-23 14:47:23 +0500

    Task15 is done

--- Task0/Homework2/Task15/Program.cs
+++ Task0/Homework2/Task15/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите число от 1 до 7:");
+int number = Convert.ToInt32(ReadLine());
+if(number>7)
+{
+    WriteLine("Введено неверное число");
+}
+if(number<6)
+{
+    WriteLine("нет");
+}
+if(number==6|number==7)
+{
+    WriteLine("Выходной!");
+}
