8e9573485d83031ce0ed349ac943e3abd8479779
Ksenia <knopik-ksu@mail.ru>
2022-12-23 14:44:22 +0500

    Task13 is done

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите число: ");
+int number = Convert.ToInt32(ReadLine());
+if(number<100)
+{
+    WriteLine("Третьей цифры нет!");
+}
+else
+{
+    while(number>999)
+    {
+        number=number/10;
+    }
+    WriteLine(number%10);
+}
