84b4b18ff5d521efceb7da1d3924f4a08d44aeef
Ksenia <knopik-ksu@mail.ru>
2022-12-27 21:33:34 +0500

    Task19 done

--- Task0/Homework3/Task19/Program.cs
+++ Task0/Homework3/Task19/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+WriteLine("Введите пятизначное число:");
+int number = Convert.ToInt32(ReadLine());
+if(number > 99999 | number < 10000)
+{
+    WriteLine("Неверное число!");
+    return;
+}
+int pal = 0;
+int num = number;
+while (number>0)
+{
+    pal = pal * 10 + number % 10;
+    number /= 10;
+}
+WriteLine(num == pal? "Палиндром" : "Не палиндром");
