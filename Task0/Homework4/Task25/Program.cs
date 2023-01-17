9d38188dd38f7010375ebe689696db3a769cce36
Ksenia <knopik-ksu@mail.ru>
2023-01-17 17:08:01 +0500

    Task25 is done

--- Task0/Homework4/Task25/Program.cs
+++ Task0/Homework4/Task25/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+Write("Введите число А: ");
+int.TryParse(ReadLine(), out int a);
+Write("Введите число B: ");
+int.TryParse(ReadLine(), out int b);
+int Pow=GetPow(a,b);
+WriteLine($"{a} в {b} степени = {Pow}");
+
+int GetPow(int A, int B)
+{
+    int result = 1;
+    while(B>0)
+    {
+        result=result*A;
+        B--;
+    }
+    return result;
+}
