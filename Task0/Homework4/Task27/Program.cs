a0f07d8640d9cb29eba6c50408b7f481fb79c7c1
Ksenia <knopik-ksu@mail.ru>
2023-01-17 20:12:22 +0500

    Task27 is done

---
+++ Task0/Homework4/Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+Write("Введите число: ");
+int.TryParse(ReadLine(), out int number);
+int Sum = GetSum(number);
+WriteLine($"Сумма цифр числа {number} = {Sum}");
+
+int GetSum(int num)
+{
+    int result = 0;
+    while(num>0)
+    {
+        result = result+num%10;
+        num = num/10;
+    }
+   return result; 
+}
