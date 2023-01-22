011f7a1256751c11ad0c3ee39ab0a7c489c0e5d0
Ksenia <knopik-ksu@mail.ru>
2023-01-22 23:37:22 +0500

    Task34 is done

--- Task0/Homework5/Task34/Program.cs
+++ Task0/Homework5/Task34/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+int[]array = GetArray(4, 100, 999);
+PrintArray(array);
+ArrayCount(array);
+WriteLine();
+WriteLine($"количество четных чисел в массиве: {ArrayCount(array)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] resultArray = new int[size];
+    Random rnd = new Random();
+    for(int i=0; i<size; i++)
+    {
+        resultArray[i] = rnd.Next(minValue, maxValue+1);
+    }
+    return resultArray;
+}
+
+void PrintArray(int[] inArray)
+{
+    Write("[");
+    for(int i=0; i<inArray.Length-1; i++)
+    {
+        Write($"{inArray[i]}, ");
+    }
+    Write($"{inArray[inArray.Length-1]}]");
+}
+
+int ArrayCount(int[]inArray2)
+{
+int result=0;
+int number=0;
+foreach(int a in array)
+{
+    number=a%2;
+    if(number==0) result++;
+}
+return result;
+}
