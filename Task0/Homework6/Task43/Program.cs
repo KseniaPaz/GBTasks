3ef1193536ffe8b5797282371125cd633025dc3f
Ksenia <knopik-ksu@mail.ru>
2023-01-25 20:44:44 +0500

    Task43 is done

--- Task0/Homework6/Task43/Program.cs
+++ Task0/Homework6/Task43/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+var k1=AskUserForNumber("Введите k1:");
+var b1=AskUserForNumber("Введите b1:");
+var k2=AskUserForNumber("Введите k2:");
+var b2=AskUserForNumber("Введите b2:");
+
+var x = -(b1 - b2) / (k1 - k2);
+var y = k1 * x + b1;
+x = Math.Round(x, 3);
+y = Math.Round(y, 3);
+
+WriteLine($"Точка пересечения двух прямых: ({x};{y})");
+
+
+double AskUserForNumber(string inPrompt)
+{
+    double resultNum=0;
+    string userInput=" ";
+
+    while(!double.TryParse(userInput, out resultNum))
+    {
+        Console.Write(inPrompt);
+        userInput=Console.ReadLine()!;
+    }
+    return resultNum;
+}
