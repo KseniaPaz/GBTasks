8a83151e91cf010e8cd8da449981dbaa8521ca5f
Ksenia <knopik-ksu@mail.ru>
2023-01-25 18:23:37 +0500

    Task41 is done

---
+++ Task0/Homework6/Task41/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+Write("Введите элементы(через пробел): ");
+int[] array = Array.ConvertAll(ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries), int.Parse);
+ArrayCount(array);
+WriteLine();
+WriteLine($"количество чисел больше 0: {ArrayCount(array)}");
+
+int ArrayCount(int[]inArray)
+{
+int result=0;
+for(int i=0; i<inArray.Length; i++)
+{
+    if(inArray[i]>0) result++;
+} 
+return result;
+}
