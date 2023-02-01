b389b638b47b6b2f5e2bcf1d59519e36eb37754d
Ksenia <knopik-ksu@mail.ru>
2023-02-01 18:06:50 +0500

    Task68 is done

--- Task0/Homework9/Task68/Program.cs
+++ Task0/Homework9/Task68/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите первое неотрицательное число m: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Write("Введите второе неотрицательное число n: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int m, int n)
+{
+if (m == 0) return n + 1;
+else if (n == 0) return Ackerman (m - 1, 1);
+return Ackerman (m - 1, Ackerman (m, n - 1));
+}
+int result = Ackerman(num1, num2);
+WriteLine($"A(m,n) = {result}");
