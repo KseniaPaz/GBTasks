4e215739cd12c165c2376ed79576ae20d144b106
Ksenia <knopik-ksu@mail.ru>
2023-02-01 17:54:59 +0500

    Task64 is done

--- Task0/Homework9/Task64/Program.cs
+++ Task0/Homework9/Task64/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число N: ");
+int number = Convert.ToInt32(ReadLine());
+WriteLine(GetNumbers(number));
+
+
+string GetNumbers(int num)
+{
+    if (num == 1) return num.ToString();
+    else return $"{num},{GetNumbers(num - 1)}";
+}
