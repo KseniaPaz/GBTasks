80ad69c425fba67c204d0412fbf13a278277c621
Ksenia <knopik-ksu@mail.ru>
2023-02-01 18:00:50 +0500

    Task66 is done

--- Task0/Homework9/Task66/Program.cs
+++ Task0/Homework9/Task66/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите число M: ");
+int M = Convert.ToInt32(ReadLine());
+Write("Введите число N: ");
+int N = Convert.ToInt32(ReadLine());
+
+int SumNumbers(int minValue, int maxValue)
+{
+    if(minValue == maxValue) return maxValue;
+    return SumNumbers(minValue + 1, maxValue) + minValue;
+}
+int sum = SumNumbers(M, N);
+WriteLine($"Сумма натуральных элементов в промежутке от {M} до {N}  = {sum}");
